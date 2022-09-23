e115f6a4109bbe06f369518ea3742ecd87aedb2b
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-23 19:37:53 -0300

    add: Controller para os pedidos recebidos

---
+++ McDonaldsTest/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace McDonaldsTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PostOrder()
+        {
+            return StatusCode((int)HttpStatusCode.OK, new { message = "Order posted" });
+        }
+    }
+}
