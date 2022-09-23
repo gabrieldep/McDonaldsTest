7b9ec941d06d7f7e7bd7b51b8ea41fd1ddfd4cba
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-23 19:37:34 -0300

    Add: Service para colocar os metodos de comunicacao com o rabbit mq

---
+++ McDonaldsTest/Services/RabbitMqService.cs
@@ -0,0 +1,34 @@
+﻿using McDonaldsTest.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace McDonaldsTest.Services
+{
+    public class RabbitMqService
+    {
+        public static void SendOrderToQueue(Order order)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "http://localhost:15672/"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: order.KitechenArea.ToString(),
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            string message = JsonConvert.SerializeObject(order);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: order.GUID,
+                                 basicProperties: null,
+                                 body: body);
+        }
+    }
+}
