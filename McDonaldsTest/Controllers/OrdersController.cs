d60a4e5ee91a2003e96e212dd58cb9ff29bf0321
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-23 23:58:26 -0300

    Recebendo um orderDTO e adicionando campos corretos

--- McDonaldsTest/Controllers/OrdersController.cs
+++ McDonaldsTest/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
-﻿using McDonaldsTest.Models;
+﻿using McDonaldsTest.DTOs;
+using McDonaldsTest.Models;
 using McDonaldsTest.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +12,18 @@ namespace McDonaldsTest.Controllers
     public class OrdersController : ControllerBase
     {
         [HttpPost("PostOrder")]
-        public IActionResult PostOrder([FromBody] Order order, string token)
+        public IActionResult PostOrder([FromBody] OrderDTO orderDTO, string token)
         {
             if (token != "7e5706d9-c662-4032-92a2-d6a787a3f95b")
                 return StatusCode((int)HttpStatusCode.Unauthorized);
+            var order = new Order()
+            {
+                GUID = Guid.NewGuid().ToString(),
+                Received = DateTime.Now,
+                ClientIdentifier = orderDTO.ClientIdentifier,
+                OrderDetail = orderDTO.OrderDetails,
+                KitechenArea = Enums.KitechenArea.Drink
+            };
             RabbitMqService.SendOrderToQueue(order);
             return StatusCode((int)HttpStatusCode.OK, new { message = "Order posted" });
         }
