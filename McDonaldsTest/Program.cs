68e674f36cb1608e2227f5b65b3714be90b9b1eb
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-25 13:23:31 -0300

    Add: dependency injection

--- McDonaldsTest/Program.cs
+++ McDonaldsTest/Program.cs
@@ -1,3 +1,6 @@
+using McDonaldsTest.Interfaces;
+using McDonaldsTest.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IMessageSender, RabbitMqSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
