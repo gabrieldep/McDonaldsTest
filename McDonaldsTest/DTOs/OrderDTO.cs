5cc9ba0e65944f9b6d60adc40d02808436de006e
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-25 13:22:12 -0300

    Add: Kitchen area no order dto

--- McDonaldsTest/DTOs/OrderDTO.cs
+++ McDonaldsTest/DTOs/OrderDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace McDonaldsTest.DTOs
+﻿using McDonaldsTest.Models;
+
+namespace McDonaldsTest.DTOs
 {
     public class OrderDTO
     {
         public string? OrderDetails { get; set; }
         public string? ClientIdentifier { get; set; }
+        public Enums.KitchenArea KitchenArea { get; set; }
     }
 }
