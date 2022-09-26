d90a935e7c686ff14e154af8930b159876d5c5ca
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-26 18:30:33 -0300

    Add: Outros testes

--- TestProject/Tests/OrdersTest.cs
+++ TestProject/Tests/OrdersTest.cs
@@ -16,13 +16,18 @@ namespace TestProject.Tests
 {
     public class OrdersTest
     {
+        private readonly HttpClient _httpClient;
+        public OrdersTest()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            _httpClient = webAppFactory.CreateClient();
+            _httpClient.DefaultRequestHeaders.Add("token", "7e5706d9-c662-4032-92a2-d6a787a3f95b");
+        }
+
         [Fact]
-        public static async Task Test1Async()
+        public async Task Test1Async()
         {
             //Arrange
-            var webAppFactory = new WebApplicationFactory<Program>();
-            var httpClient = webAppFactory.CreateClient();
-            httpClient.DefaultRequestHeaders.Add("token", "7e5706d9-c662-4032-92a2-d6a787a3f95b");
             var order = new OrderDTO()
             {
                 KitchenArea = Enums.KitchenArea.Fries,
@@ -34,10 +39,52 @@ namespace TestProject.Tests
                                         Encoding.UTF8, "application/json");
 
             //Act
-            var response = await httpClient.PostAsync("api/Orders/PostOrder", content);
+            var response = await _httpClient.PostAsync("api/Orders/PostOrder", content);
 
             //Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Test2Async()
+        {
+            //Arrange
+            var order = new OrderDTO()
+            {
+                KitchenArea = Enums.KitchenArea.Fries,
+                ClientIdentifier = "",
+                OrderDetails = "Cheddar McMelt and Fries"
+            };
+            JObject document = JObject.FromObject(order);
+            StringContent content = new(JsonConvert.SerializeObject(document),
+                                        Encoding.UTF8, "application/json");
+
+            //Act
+            var response = await _httpClient.PostAsync("api/Orders/PostOrder", content);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Test3Async()
+        {
+            //Arrange
+            var order = new OrderDTO()
+            {
+                KitchenArea = Enums.KitchenArea.Fries,
+                ClientIdentifier = "",
+                OrderDetails = null
+            };
+            JObject document = JObject.FromObject(order);
+            StringContent content = new(JsonConvert.SerializeObject(document),
+                                        Encoding.UTF8, "application/json");
+
+            //Act
+            var response = await _httpClient.PostAsync("api/Orders/PostOrder", content);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
     }
 }
