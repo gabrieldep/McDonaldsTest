be11c9bd3676aeaaaa8f5aa5b9a7e14319eb3f0d
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-26 13:12:35 -0300

    Verificacoes

--- McDonaldsTest/DTOs/OrderDTO.cs
+++ McDonaldsTest/DTOs/OrderDTO.cs
@@ -1,11 +1,15 @@
 ﻿using McDonaldsTest.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace McDonaldsTest.DTOs
 {
     public class OrderDTO
     {
+        [Required(AllowEmptyStrings = false)]
         public string? OrderDetails { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string? ClientIdentifier { get; set; }
+        [Required]
         public Enums.KitchenArea KitchenArea { get; set; }
     }
 }
